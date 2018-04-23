f3263d2593e6b6b1d50da24fb01601451b4aa429
CptKukiFasz <38591918+CptKukiFasz@users.noreply.github.com>
2018-04-23 02:30:10 +0200

    Update Movable.java

--- Szkeleton/src/Movable.java
+++ Szkeleton/src/Movable.java
@@ -1,11 +1,15 @@
 import java.io.FileWriter;
 import java.io.IOException;
 
+//Egy mozgatható dolgot reprezentál.
 public abstract class Movable {
 
 	protected Field field;
 	protected int forceRemaining;
-
+	
+	//Egy sima setter függvényhez képest annyi plusz,
+	//hogy bool a visszatérése. Ha fR < 0,
+	//akkor false a visszatérés, egyébként beállítja, és true.
 	public boolean setForceRemaining(int f) {
 		forceRemaining = f;
 		return true;
@@ -26,7 +30,9 @@ public abstract class Movable {
 	public void printMovable(FileWriter output) throws IOException {
 		;
 	}
-
+	//A mozgatható dolog képes arra, hogy meghaljon, 
+	//egy láda beleeshet a lyukba, úgy ahogy, a munkás is.
+	//Vagy az is előfordulhat, hogy a munkást összelapítják egy doboz segítségével.
 	public abstract void die();
 
 	public abstract boolean isCrate();
