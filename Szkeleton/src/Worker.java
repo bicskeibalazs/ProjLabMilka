4a4ea5fd14738a991102ed3aa33bda5b02574182
CptKukiFasz <38591918+CptKukiFasz@users.noreply.github.com>
2018-04-23 02:49:12 +0200

    Update Worker.java

--- Szkeleton/src/Worker.java
+++ Szkeleton/src/Worker.java
@@ -1,14 +1,22 @@
 import java.io.FileWriter;
 import java.io.IOException;
 
+//Képes arra, hogy egy adott irányba mozogjon, és meghaljon. Munkásokat reprezentálja a raktárban.
 public class Worker extends Movable {
 
 	private int strength;
+	//A dolgozó életben van-e.
 	private boolean alive;
+	//Azt vizsgálja egy mozgás során, hogy őt tolták-e (egy ládával)
+	//vagy ő az, aki az egész mozgás iterakciót kezdeményezte.
 	private boolean beingPushed;
+	// A dolgozó felhasználta-e már a nála lévő mézet.
 	private boolean honeyDeployed;
+	// A dolgozó felhasználta-e már a nála lévő olajat.
 	private boolean oilDeployed;
+	//A raktárat testesíti meg, ahol a munkások a ládákat tolják.
 	private Warehouse warehouse;
+	//A munkás színe
 	private Color color;
 
 	public Worker(Field f, Color c, Warehouse w) {
@@ -71,7 +79,8 @@ public class Worker extends Movable {
 	public boolean getOilDeployed() {
 		return oilDeployed;
 	}
-
+	//Létrehoz egy új Oil objektumot,
+	//és beállítja a field-jének a surface attribútumára.
 	public void DeployOil() {
 		if (alive) {
 			field.setSurface(new Oil(field));
@@ -79,7 +88,9 @@ public class Worker extends Movable {
 		}
 
 	}
-
+	
+	// Létrehoz egy új Honey objektumot,
+	//és beállítja a field-jének a surface attribútumára.
 	public void DeployHoney() {
 		if (alive) {
 			field.setSurface(new Honey(field));
@@ -90,12 +101,14 @@ public class Worker extends Movable {
 	public boolean isBeingPushed() {
 		return beingPushed;
 	}
-
+	
+	// A dolgozót az adott irányban szomszédos mezőre lépteti
 	public void move(Direction d) {
 		if (alive)
 			this.getField().wantsToMoveFrom(d);
 	}
-
+	
+	//Törli a dolgozót a saját mezőjéről (és a teljes raktárból).
 	public void die() {
 		alive = false;
 	}
