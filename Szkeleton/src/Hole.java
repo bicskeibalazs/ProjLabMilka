e2eedf3bcddaf2cfd404ba50d1878e0313c102b1
CptKukiFasz <38591918+CptKukiFasz@users.noreply.github.com>
2018-04-23 02:27:39 +0200

    Update Hole.java

--- Szkeleton/src/Hole.java
+++ Szkeleton/src/Hole.java
@@ -1,12 +1,15 @@
 import java.io.FileWriter;
 import java.io.IOException;
 
+//Ha Movable kerül rá akkor azt megöli(die). (Örökölt felelőssége: ld. Field.)
 public class Hole extends Field {
 
+	//Megöli az erre a mezőre mozgó Movable-t.
 	public boolean wantsToMoveHere(Direction d, Movable m) {
 		return true;
 	}
-
+	
+	//Kiíró függvény
 	public void printField(FileWriter output) throws IOException {
 		output.write('H');
 	}
