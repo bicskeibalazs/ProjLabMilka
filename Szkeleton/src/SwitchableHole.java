0da59ae6d37dba6807a717f0a6c6f4429df314b1
CptKukiFasz <38591918+CptKukiFasz@users.noreply.github.com>
2018-04-23 02:42:52 +0200

    Update SwitchableHole.java

--- Szkeleton/src/SwitchableHole.java
+++ Szkeleton/src/SwitchableHole.java
@@ -1,9 +1,12 @@
 import java.io.FileWriter;
 import java.io.IOException;
 
+//Lyukként vagy sima mezőként viselkedik. (Örökölt felelősség: ld. Hole.)
 public class SwitchableHole extends Hole {
-
+	
+	//A kapcsolható lyukhoz tartozó kapcsoló.
 	private Switch _switch;
+	//A kapcsolható lyuk állapota. Lehet aktív vagy inaktív.
 	private boolean state;
 	private int number;
 
@@ -31,7 +34,8 @@ public class SwitchableHole extends Hole {
 	public void setState(boolean b) {
 		state = b;
 	}
-
+	
+	//A kapcsolható lyukat aktív állapotba helyezi.
 	public void activate() {
 		state = true;
 		if (isOccupied()) {
@@ -40,18 +44,23 @@ public class SwitchableHole extends Hole {
 			movable = null;
 		}
 	}
-
+	
+	//A kapcsolható lyukat inaktív állapotba helyezi.
 	public void deactivate() {
 		state = false;
 	}
-
+	
+	//Ha aktív állapotban van a kapcsolható lyuk,
+	//akkor az erre a mezőre mozgó Movable-t megöli.
+	//Ha inaktív állapotban van, akkor erre a mezőre lépteti a movable-t.
 	public boolean wantsToMoveHere(Direction d, Movable m) {
 		if (state)
 			return true;
 		else
 			return super.wantsToMoveHere(d, m);
 	}
-
+	
+	//Kiíró függvény
 	public void printField(FileWriter output) throws IOException {
 		output.write('K');
 		output.write(number);
