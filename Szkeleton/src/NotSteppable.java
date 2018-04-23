6c3fbf7a55dc2ae6e3b2e311397983439a78e54c
CptKukiFasz <38591918+CptKukiFasz@users.noreply.github.com>
2018-04-23 02:33:04 +0200

    Update NotSteppable.java

--- Szkeleton/src/NotSteppable.java
+++ Szkeleton/src/NotSteppable.java
@@ -3,11 +3,13 @@ import java.io.IOException;
 
 //Egy olyan speciális mezőt reprezentál, amelyre nem kerülhet movable. (Örökölt felelősség: ld. Field.)
 public class NotSteppable extends Field {
-
+	
+	//Nem engedi, hogy egy Movable erre a speciális mezőre lépjen.
 	public boolean wantsToMoveHere(Direction d, Movable m) {
 		return false;
 	}
-
+	
+	//Kiíró függvény
 	public void printField(FileWriter output) throws IOException {
 		output.write('N');
 	}
